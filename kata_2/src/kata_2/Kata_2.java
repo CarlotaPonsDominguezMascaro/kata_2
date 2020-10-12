7858f130e08834c71d900c9955d88dd5e077fe76
Carlota Pons <carlotaponsdm@gmail.com>
2020-10-12 18:11:08 +0100

    kata2 verson 3

--- kata_2/src/kata_2/Kata_2.java
+++ kata_2/src/kata_2/Kata_2.java
@@ -1,24 +1,17 @@
 package kata_2;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata_2 {
 
     public static void main(String[] args) {
         int[] data = {1,1,2,3,4,1,2,2,3,5,1,9,8,7,6};
-        Map<Integer,Integer> histogram = new HashMap();
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-            /*
-            Esta expresión se traduce a lenguaje natural así: “Si el histograma contiene la clave key,
-            rellena el mapa con el valor correspondiente y súmale 1, y si no contiene la clave key, 
-            rellena el mapa con el valor 1.
-            */
-        }
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
         
-        for (Object key : histogram.keySet()) {
-            System.out.println(key + "==>" + histogram.get(key));
+        
+        for (Object key : histogr.keySet()) {
+            System.out.println(key + "==>" + histogr.get(key));
         }
     }
     
